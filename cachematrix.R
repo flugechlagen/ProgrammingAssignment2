4ad8ca0df72ddc2f658d9c878ea847edf5ffa2cd
flugechlagen <jamesragudo3@gmail.com>
2020-08-19 20:54:55 +0800

    My R assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,25 @@
 ## functions do
 
 ## Write a short comment describing this function
+# so this program 
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  q <- NULL
+  set <- function(y){
+    x <<- y
+    q <<- NULL
+    
+  }
+  
+  get <- function() x
+  set_Inverse <- function(inverse) q <<- inverse
+  get_Inverse <- function() q 
+  
+  list(set = set, get = get, 
+       set_Inverse = set_Inverse, 
+       get_Inverse = get_Inverse)
 
 }
 
@@ -12,4 +29,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  q <- x$get_Inverse()
+  
+  if(is.null(q) == FALSE){
+  
+    message("Getting the data")
+    return(q)
+  }
+  
+  solb <- x$get()
+  q <- solve(solb,...)
+  x$set_Inverse(q)
+  q
+  
 }
